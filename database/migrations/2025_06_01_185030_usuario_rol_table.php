3127f9541d17ddf99a3c6218b5236b8aa2635fab
Juan Del Pozo <juandelpozo412@gmail.com>
2025-06-01 17:39:12 -0300

    commit migraciones bd

---
+++ database/migrations/2025_06_01_185030_usuario_rol_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('usuario_rol', function (Blueprint $table){
+        $table->id();
+        $table->unsignedBigInteger('usuario_id')->nullable();
+        $table->unsignedBigInteger('rol_id')->nullable();
+        $table->foreign('usuario_id')->references('id')->on('usuarios')->onDelete('set null');
+        $table->foreign('rol_id')->references('id')->on('rols')->onDelete('set null');
+        $table->timestamps();
+    });
+}
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+                Schema::dropIfExists('usuario_rol');
+
+    }
+};
